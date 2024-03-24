9441f09375a82f08f4e95c655d619e92dfb5c14c
Pablo M <pablo@maldonado.cz>
2024-03-24 17:00:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,6 @@
 
 using namespace std;
 
-void readLabels(const char *labelsFile, std::vector<std::string> &labels)
-{
-    std::string line;
-    std::ifstream fin(labelsFile);
-    while (getline(fin, line)) {
-        labels.push_back(line);
-    }
-}
-
 int main(int argc,char ** argv) {
 
     if (argc != 4)
@@ -32,9 +23,20 @@ int main(int argc,char ** argv) {
     const char *IMG_FILE = argv[3];
     std::vector<float> predictions;
 
-//    std::vector<std::string> labels;
-    string labels[7] = {"10c", "10d", "10h", "10s", "2c", "2d","2h"};
-    //readLabels(LABELS_FILE, labels);
+    string labels[53] = {"10c", "10d", "10h", "10s", 
+                        "2c", "2d","2h", "2s",
+                        "3c", "3d","3h", "3s",
+                        "4c", "4d","4h", "4s",
+                        "5c", "5d","5h", "5s",
+                        "6c", "6d","6h", "6s",
+                        "7c", "7d","7h", "7s",
+                        "8c", "8d","8h", "8s",
+                        "9c", "9d","9h", "9s",
+                        "Ac", "Ad","Ah", "As",
+                        "Jc", "Jd","Jh", "Js",
+                        "Kc", "Kd","Kh", "Ks",
+                        "Qc", "Qd","Qh", "Qs","Zx"};
+
 
     auto input = cppflow::decode_jpeg(cppflow::read_file(std::string(IMG_FILE)));
 
